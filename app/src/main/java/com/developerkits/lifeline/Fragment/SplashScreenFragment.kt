2f5707c3fd8217d9952992db2a848c09427b21a5
Akash Das <akashdas.dev@gmail.com>
2023-09-23 14:00:12 +0600

    add fragment

---
+++ app/src/main/java/com/developerkits/lifeline/Fragment/SplashScreenFragment.kt
@@ -0,0 +1,20 @@
+package com.developerkits.lifeline.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.developerkits.lifeline.R
+
+class SplashScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+}
