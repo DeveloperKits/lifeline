df4bb22601c71b244e4336ff0fc992f773fcb74f
Akash Das <akashdas.dev@gmail.com>
2023-10-09 14:24:00 +0600

    added some UI

--- app/src/main/java/com/developerkits/lifeline/MainActivity.kt
+++ app/src/main/java/com/developerkits/lifeline/MainActivity.kt
@@ -2,10 +2,34 @@ package com.developerkits.lifeline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.developerkits.lifeline.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        //binding.bottomNavigation.setupWithNavController(navController)
+
+        // If you have some destinations where you don't want to show the BottomNavigationView:
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment -> binding.bottomNavigation.visibility = View.VISIBLE
+                else -> binding.bottomNavigation.visibility = View.GONE
+            }
+        }
     }
 }
