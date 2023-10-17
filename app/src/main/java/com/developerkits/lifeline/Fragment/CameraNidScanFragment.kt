196e8c204c337f2009b64524a0251d126ebac0c9
Akash Das <akashdas.dev@gmail.com>
2023-10-17 21:59:34 +0600

    added camerax

--- app/src/main/java/com/developerkits/lifeline/Fragment/CameraNidScanFragment.kt
+++ app/src/main/java/com/developerkits/lifeline/Fragment/CameraNidScanFragment.kt
@@ -1,31 +1,59 @@
 package com.developerkits.lifeline.Fragment
 
 import android.Manifest
+import android.content.ContentValues
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Camera
+import android.net.Uri
+import android.opengl.Visibility
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.AspectRatio
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.FocusMeteringAction
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
+import androidx.camera.core.SurfaceOrientedMeteringPointFactory
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
-import com.developerkits.lifeline.R
 import com.developerkits.lifeline.databinding.FragmentCameraNidScanBinding
+import java.io.File
+import java.net.URI
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 class CameraNidScanFragment : Fragment() {
 
     private lateinit var binding: FragmentCameraNidScanBinding
+    private lateinit var cameraExecutor: ExecutorService
+    private var imageCapture: ImageCapture? = null
+    private val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+    private lateinit var savedUri: Uri
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentCameraNidScanBinding.inflate(inflater, container, false)
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
         // Inflate the layout for this fragment
         return binding.root
     }
@@ -34,14 +62,31 @@ class CameraNidScanFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         if (allPermissionsGranted()) {
-            startCamera()
+            startCamera(binding.viewFinder)
+            startCamera(binding.centeredViewFinder)
         } else {
             activityResultLauncher.launch(REQUIRED_PERMISSIONS)
         }
+
+        binding.submit.setOnClickListener {
+            if (binding.submit.text == "Capture") {
+                captureImage()
+            } else if (binding.submit.text == "Submit") {
+                // Todo: pass bitmap in another fragment
+            }
+        }
+
+        binding.retake.setOnClickListener{
+            binding.previewImage.visibility = View.GONE
+            binding.submit.text = "Capture"
+        }
+
     }
 
-    private fun startCamera() {
+    private fun startCamera(viewFinder: PreviewView) {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        imageCapture = ImageCapture.Builder().setTargetAspectRatio(AspectRatio.RATIO_4_3).build()
 
         cameraProviderFuture.addListener({
             // Used to bind the lifecycle of cameras to the lifecycle owner
@@ -49,9 +94,10 @@ class CameraNidScanFragment : Fragment() {
 
             // Preview
             val preview = Preview.Builder()
+                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
                 .build()
                 .also {
-                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                    it.setSurfaceProvider(viewFinder.surfaceProvider)
                 }
 
             // Select back camera as a default
@@ -63,7 +109,7 @@ class CameraNidScanFragment : Fragment() {
 
                 // Bind use cases to camera
                 cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview)
+                    this, cameraSelector, preview, imageCapture)
 
             } catch(exc: Exception) {
                 //Log.e(TAG, "Use case binding failed", exc)
@@ -72,23 +118,66 @@ class CameraNidScanFragment : Fragment() {
         }, ContextCompat.getMainExecutor(requireContext()))
     }
 
+    private fun captureImage() {
+        val imageCapture = imageCapture ?: return
+
+        imageCapture.takePicture(
+            ContextCompat.getMainExecutor(requireContext()),
+            object : ImageCapture.OnImageCapturedCallback() {
+                override fun onCaptureSuccess(image: ImageProxy) {
+                    val buffer = image.planes[0].buffer
+                    val bytes = ByteArray(buffer.remaining())
+                    buffer.get(bytes)
+                    //Todo : There image crop doesn't work and also some problem have in view
+                    val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                    val croppedBitmap = cropBitmap(bitmap, 250, 200)
+                    binding.previewImage.visibility = View.VISIBLE
+                    binding.previewImage.setImageBitmap(croppedBitmap)
+                    binding.previewImage.rotation = 90F
+                    binding.submit.text = "Submit"
+                    image.close()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e("TAG", "Photo capture failed: ${exception.message}", exception)
+                }
+            }
+        )
+    }
+
+    fun cropBitmap(bitmap: Bitmap, width: Int, height: Int): Bitmap {
+        val aspectRatio = width.toFloat() / height.toFloat()
+        var newWidth = bitmap.width
+        var newHeight = (bitmap.width / aspectRatio).toInt()
+        if (newHeight > bitmap.height) {
+            newHeight = bitmap.height
+            newWidth = (bitmap.height * aspectRatio).toInt()
+        }
+        val xOffset = (bitmap.width - newWidth) / 2
+        val yOffset = (bitmap.height - newHeight) / 2
+        return Bitmap.createBitmap(bitmap, xOffset, yOffset, newWidth, newHeight)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        cameraExecutor.shutdown()
+    }
+
+
+    // Camera permission
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
     }
 
     private val REQUIRED_PERMISSIONS =
         mutableListOf (
-            Manifest.permission.CAMERA,
-            Manifest.permission.RECORD_AUDIO
+            Manifest.permission.CAMERA
         ).apply {
             if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
                 add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
             }
         }.toTypedArray()
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-    }
 
     private val activityResultLauncher = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
@@ -104,10 +193,10 @@ class CameraNidScanFragment : Fragment() {
                 Toast.LENGTH_SHORT
             ).show()
         } else {
-            startCamera()
+            startCamera(binding.viewFinder)
+            startCamera(binding.centeredViewFinder)
         }
     }
-
 
 
 }
