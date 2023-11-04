19759736d873ea370f5bcaa8c8b25acb4ab6f682
Akash Das <akashdas.dev@gmail.com>
2023-11-05 00:21:16 +0600

    capture and convert
    capture, convert and check actual info or not then inform user

--- app/src/main/java/com/developerkits/lifeline/Fragment/CameraNidScanFragment.kt
+++ app/src/main/java/com/developerkits/lifeline/Fragment/CameraNidScanFragment.kt
@@ -1,18 +1,14 @@
 package com.developerkits.lifeline.Fragment
 
 import android.Manifest
-import android.content.ContentValues
+import android.R.attr
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.Camera
 import android.net.Uri
-import android.opengl.Visibility
 import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,23 +16,24 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.AspectRatio
 import androidx.camera.core.CameraSelector
-import androidx.camera.core.FocusMeteringAction
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
-import androidx.camera.core.SurfaceOrientedMeteringPointFactory
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.developerkits.lifeline.R
 import com.developerkits.lifeline.databinding.FragmentCameraNidScanBinding
-import java.io.File
-import java.net.URI
-import java.text.SimpleDateFormat
-import java.util.Locale
+import com.google.android.material.snackbar.Snackbar
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
+
 
 class CameraNidScanFragment : Fragment() {
 
@@ -62,7 +59,6 @@ class CameraNidScanFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         if (allPermissionsGranted()) {
-            startCamera(binding.viewFinder)
             startCamera(binding.centeredViewFinder)
         } else {
             activityResultLauncher.launch(REQUIRED_PERMISSIONS)
@@ -130,11 +126,14 @@ class CameraNidScanFragment : Fragment() {
                     buffer.get(bytes)
                     //Todo : There image crop doesn't work and also some problem have in view
                     val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-                    val croppedBitmap = cropBitmap(bitmap, 250, 200)
                     binding.previewImage.visibility = View.VISIBLE
-                    binding.previewImage.setImageBitmap(croppedBitmap)
+                    binding.previewImage.setImageBitmap(bitmap)
                     binding.previewImage.rotation = 90F
                     binding.submit.text = "Submit"
+                    
+                    // convert image to text and also if image are not clear then show a notification
+                    convertToText(bitmap)
+                    
                     image.close()
                 }
 
@@ -145,17 +144,73 @@ class CameraNidScanFragment : Fragment() {
         )
     }
 
-    fun cropBitmap(bitmap: Bitmap, width: Int, height: Int): Bitmap {
-        val aspectRatio = width.toFloat() / height.toFloat()
-        var newWidth = bitmap.width
-        var newHeight = (bitmap.width / aspectRatio).toInt()
-        if (newHeight > bitmap.height) {
-            newHeight = bitmap.height
-            newWidth = (bitmap.height * aspectRatio).toInt()
+    private fun convertToText(bitmap: Bitmap?) {
+        // Assuming 'bitmap' is your Bitmap object
+        val inputImage = InputImage.fromBitmap(bitmap!!, 0) // 0 for rotation degrees
+
+        val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+        recognizer.process(inputImage)
+            .addOnSuccessListener { text ->
+                val recognizedText = text.text
+
+                // Check for the presence of "National ID Card" in the text
+                if (!recognizedText.contains("National ID Card", ignoreCase = true)) {
+                    Snackbar.make(binding.mainLayout, "Invalid document. Please ensure you capture the correct NID card.", Snackbar.LENGTH_LONG)
+                        .setAction("Ok") {}
+                        .setTextColor(resources.getColor(R.color.white))
+                        .setBackgroundTint(resources.getColor(R.color.app_main_color))
+                        .setActionTextColor(resources.getColor(R.color.white))
+                        .show()
+                } else {
+                    getInfoFromNID(recognizedText)
+                }
+
+                Log.i("Text Result", "Text : ${text.text}")
+            }
+            .addOnFailureListener { e ->
+                // Handle the error
+                // ...
+            }
+
+    }
+
+    private fun getInfoFromNID(text: String) {
+        val infoMap = mutableMapOf<String, String>()
+
+        // Regex patterns to match the required fields
+        val namePattern = Regex("Name:\\s*(.*)")
+        val dobPattern = Regex("Date of Birth:\\s*(\\d{2} \\w{3} \\d{4})")
+        val idNoPattern = Regex("ID NO:\\s*(\\d+)")
+
+        // Try to find matches for the first type of card
+        namePattern.find(text)?.let { infoMap["Name"] = it.groupValues[1].trim() }
+        dobPattern.find(text)?.let { infoMap["Date of Birth"] = it.groupValues[1].trim() }
+        idNoPattern.find(text)?.let { infoMap["ID No"] = it.groupValues[1].trim() }
+
+        // If not found, try to find matches for the second type of card
+        if (infoMap.isEmpty()) {
+            //val altNamePattern = Regex("(?m)^([A-Z ]+)$") // Matches a line with all caps (name in 2nd type of card)
+            val altNamePattern = Regex("Name\\s*\\n(.+)")
+            val altDobPattern = Regex("Date of Birth\\s*(\\d{2} \\w{3} \\d{4})")
+
+            altNamePattern.find(text)?.let { infoMap["Name"] = it.groupValues[1].trim() }
+            altDobPattern.find(text)?.let { infoMap["Date of Birth"] = it.groupValues[1].trim() }
+
+            // for number ... Regex to find a sequence of digits that may include spaces, with 10 or more digits in total
+            val nidNumberPattern = Regex("(?:\\d\\s*){10,}")
+            val matchResult = nidNumberPattern.find(text)
+
+            if (matchResult != null) {
+                // Remove all non-digit characters from the matched result to get the NID number
+                val nidNumber = matchResult.value.filter { it.isDigit() }
+                Log.i("NID Number", nidNumber)
+                infoMap["ID No"] = nidNumber
+            }
         }
-        val xOffset = (bitmap.width - newWidth) / 2
-        val yOffset = (bitmap.height - newHeight) / 2
-        return Bitmap.createBitmap(bitmap, xOffset, yOffset, newWidth, newHeight)
+
+        Log.i("NID Info", "Name: ${infoMap["Name"]}, " +
+                "Date of Birth: ${infoMap["Date of Birth"]}, ID No: ${infoMap["ID No"]}")
     }
 
     override fun onDestroyView() {
@@ -193,7 +248,6 @@ class CameraNidScanFragment : Fragment() {
                 Toast.LENGTH_SHORT
             ).show()
         } else {
-            startCamera(binding.viewFinder)
             startCamera(binding.centeredViewFinder)
         }
     }
