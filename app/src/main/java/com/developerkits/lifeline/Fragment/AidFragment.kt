de6ff7f097ed736bce965ab5a92cf2497a53d824
Akash Das <akashdas.dev@gmail.com>
2023-12-14 02:22:18 +0600

    some minor

--- app/src/main/java/com/developerkits/lifeline/Fragment/AidFragment.kt
+++ app/src/main/java/com/developerkits/lifeline/Fragment/AidFragment.kt
@@ -6,15 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.developerkits.lifeline.R
+import com.developerkits.lifeline.databinding.FragmentAidBinding
 
 class AidFragment : Fragment() {
+
+    private lateinit var binding: FragmentAidBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_aid, container, false)
+        binding = FragmentAidBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
--- app/src/main/java/com/developerkits/lifeline/Fragment/ProfileFragment.kt
+++ app/src/main/java/com/developerkits/lifeline/Fragment/ProfileFragment.kt
@@ -1,12 +1,15 @@
 package com.developerkits.lifeline.Fragment
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.developerkits.lifeline.R
 import com.developerkits.lifeline.databinding.FragmentProfileBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.google.firebase.firestore.firestore
@@ -37,5 +40,28 @@ class ProfileFragment : Fragment() {
                 binding.userNumber.text = "${auth.currentUser!!.phoneNumber}"
                 binding.userName.text = "${it.getString("name")}"
             }
+
+        binding.button.setOnClickListener{
+            showLogoutConfirmationDialog()
+        }
+    }
+
+    private fun showLogoutConfirmationDialog() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(resources.getString(R.string.app_name))
+            .setMessage("Are you ant to log out?")
+
+            .setNegativeButton("Yes") { dialog, which ->
+                val sharedPreferences = requireContext()
+                    .getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+
+                // Clear all data from SharedPreferences
+                sharedPreferences.edit().clear().apply()
+                findNavController().navigate(R.id.action_profileFragment_to_registrationFragment)
+            }
+            .setPositiveButton("No") { dialog, which ->
+                dialog.dismiss()
+            }
+            .show()
     }
 }
